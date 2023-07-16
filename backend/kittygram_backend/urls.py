8b65bad67058280664b302fd9f8334649c8103fb
Расим Шаймарданов <shaimardanoff92@yandex.ru>
2023-07-16 11:53:01 +0300

    запуск 15

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -9,11 +9,15 @@ router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
+def trigger_error(request):
+  division_by_zero = 1 / 0
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),  # Работа с пользователями
     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('sentry-debug/', trigger_error),
 ]
 
 if settings.DEBUG:
